a7b6dd2d9b24b62e33eb99153d983c5722df7f03
TDC <71852270+TDC0471@users.noreply.github.com>
2020-10-14 19:03:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'NzEyMzA4OTE3NjY4OTM3NzQ4.XsPrlg.Q66dhcKbyLtvguIH05KHiOoJU7A'; 
+const token = 'NzU5ODYzNTI5MjI1NzE1NzQz.X3DsDg.xtHU94wJQvWTNr8CEkBETQHMHUk'; 
 
 
 client.on('ready', () => {
@@ -13,3 +13,5 @@ client.on('message', message => {
 	if(message.content.toLowerCase().match('~where am i?')){
         message.reply("IN MY SWAMP NOW GET OUT!");}
 })
+
+client.login(token);
