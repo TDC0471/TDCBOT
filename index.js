86ff6118d054d7b2a2ff8d40fd0423dfc2423d6c
TDC <71852270+TDC0471@users.noreply.github.com>
2020-10-19 20:37:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const {token, PREFIX} = require('config.json')
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const logint = process.env.token;
-console.log(logint) 
+
+ 
 
 client.on('ready', () => {
      client.user.setActivity("~Help ~Help ~Help ~Help", { type: "PLAYING" });
