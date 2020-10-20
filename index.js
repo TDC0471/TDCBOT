c80053e5dd1a346327b614c1f1d9ddcbfbc8683b
TDC <71852270+TDC0471@users.noreply.github.com>
2020-10-20 19:30:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
+const token = process.env.token;
  
 
 client.on('ready', () => {
@@ -18,4 +18,6 @@ if(message.content === '!HI'){message.channel.send('work')}
 });
 
 
-client.login('NzU5ODYzNTI5MjI1NzE1NzQz.X3DsDg.MWTJpicwoUDu7c05FeOJgTi_thc');
+client.login(token);
+
+//'NzU5ODYzNTI5MjI1NzE1NzQz.X3DsDg.MWTJpicwoUDu7c05FeOJgTi_thc'
