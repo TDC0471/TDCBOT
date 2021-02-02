d8f47c0a5072cfaa962d84b74a0f65756d872876
TDC <71852270+TDC0471@users.noreply.github.com>
2021-02-02 10:58:23 -0500

    Fix Invite link

--- commands/invite.js
+++ commands/invite.js
@@ -7,6 +7,6 @@ module.exports = {
     usage: ';invite',
     execute(client, message, args) {
     const Discord = require('discord.js');
-    const invitebed = new Discord.MessageEmbed().setColor("#000000").setTitle('Invite page!').addField('Link Below','[To invite press here](https://discord.com/oauth2/authorize?client_id=724766895521398827&scope=bot&permissions=1073217023)').addField('Join our support server!','[To Join press here](https://discord.com/oauth2/authorize?client_id=759862871999381536&scope=bot&permissions=2146958847)')
+    const invitebed = new Discord.MessageEmbed().setColor("#000000").setTitle('Invite page!').addField('Link Below','[To invite press here](https://discord.com/oauth2/authorize?client_id=806187538867421255&scope=bot&permissions=2147483647)')
     message.channel.send(invitebed)}  
     }
